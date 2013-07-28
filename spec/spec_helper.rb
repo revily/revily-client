5ca06bfa7b6a8136c775f72df62b20ece4fca81e
Dan Ryan <scriptfu@gmail.com>
2013-07-28 01:20:19 -0400

    fix spec_helper[

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,35 @@
-require 'rspec'
-require 'webmock/rspec'
-
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.use_transactional_fixtures = false
-  config.infer_base_class_for_anonymous_controllers = false
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.order = "random"
-  config.run_all_when_everything_filtered = true
-  config.filter_run focus: true
-  config.filter_run_excluding external: true
+unless ENV['CI']
+  require 'spork'
 end
 
-def fixture_path
-  File.expand_path("../support/fixtures", __FILE__)
+def configure
+  $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+  require 'revily/client'
+
+  require 'rspec'
+  require 'webmock/rspec'
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = false
+    config.infer_base_class_for_anonymous_controllers = false
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.order = 'random'
+    config.run_all_when_everything_filtered = true
+    config.filter_run focus: true
+    config.filter_run_excluding external: true
+  end
 end
 
-def fixture
-  File.new(fixture_path + '/' + file)
+def run
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 end
 
-def json_response(file)
-  {
-    body: fixture(file),
-    headers: {
-      content_type: 'application/json; charset=utf-8'
-    }
-  }
+if defined?(Spork)
+  Spork.prefork { configure }
+  Spork.each_run { run }
+else
+  configure
+  run
 end
